55d578be4a77419b15a3581946f7181c3a3374e4
Steve Ordahl <stordahl@microsoft.com>
2022-10-04 11:22:41 -0400

    Split bundles with more than 500 resources

---
+++ FHIRBulkImport/TransientError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FHIRBulkImport
+{
+    class TransientError : Exception
+    {
+        public TransientError(string message) :base (message)
+        {
+           
+        }
+    }
+}
